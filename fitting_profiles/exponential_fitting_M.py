d1183b867f4cbae3637f344e907be0c17dcdf851
Meinte <meinte.ringia@student.uva.nl>
2021-08-23 13:40:33 +0200

    exponential_fit_M now approximates better real load (if only its just by eye...)

--- fitting_profiles/exponential_fitting_M.py
+++ fitting_profiles/exponential_fitting_M.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-from scipy.optimize import curve_fit
-import pandas as pd
+# from scipy.optimize import curve_fit
+# import pandas as pd
 import numpy as np
-from scipy.optimize import fsolve, minimize
+from scipy.optimize import fsolve  # , minimize
 
 from load_data_scripts import load_save_profiles
 from load_data_scripts import save_unique_profile_customer
@@ -21,19 +21,34 @@ def drop_leap_day(df):
 profiles = drop_leap_day(profiles)
 
 
-def optimal_parameters(profile_np, customer_np):
+def optimal_parameters(profile, customer):
 
     def exp_eq(x):
         a, b = x
-        SJV = customer_np.sum()
-        SJV_error = SJV*(a*profile_np.pow(b).sum() - 1)
-        max_error = SJV*(np.amax(a*profile_np.pow(b)) -
-                         np.amax(customer_np)/SJV)
+        SJV = customer.sum()
+        SJV_error = SJV*(a*profile.pow(b).sum() - 1)
+        max_error = SJV*(a*profile.max()**b -
+                         customer.max()/SJV)
         return (SJV_error, max_error)
 
-    a, b = fsolve(exp_eq, (0.9, 0.9), factor=0.3, xtol=1.3e-10,
-                  maxfev=800)
+    # max_ratio = (np.amax(customer_np)/customer_np.sum())/np.amax(profile_np)
+    print('max profile: ', profile.max())
+    print('max customer: ', customer.max())
+    SJV = customer.sum()
+    guess_b = np.log(customer.max()/SJV)/np.log(profile.max())
+    guess_a = customer.sum()/profile.pow(guess_b).sum()
+    guess = (guess_a, guess_b)
 
+    print('initial guesses (a,b); ', guess)
+
+    a, b = fsolve(exp_eq, guess, factor=0.3)
+    # guess = (1.0, 1.0)
+    # a, b = diagbroyden(exp_eq, guess)
+    # x = Symbol('x')
+    # a, b = solve(exp_eq, x)
+
+    print('result of error function(should be around (0,0)): ')
+    print(exp_eq((a, b)))
     return a, b
 
 
@@ -42,18 +57,33 @@ count = 0
 better = []
 worse = []
 
-# print('connection: ', connection)
+
+def get_customer_with_profile(profile):
+    # select all customers having this profile
+    sub_customers_id = connection[connection['BASELOAD_PROFILE'] == profile]
+    sub_customers = customers[sub_customers_id]
+    return sub_customers
+
+
 for customer_id in customers.index:
     # find corresponding profile
     profile_type = connection.loc[customer_id, 'BASELOAD_PROFILE']
     print('customer {} with profile type {}'.format(customer_id, profile_type))
     profile_customer = profiles[profile_type]
     customer = customers.loc[customer_id]
+    print('this is profile: ', profile_customer.name)
+    print('with sum: ', profile_customer.sum())
+    print('and customer has SJV of: ', customer.sum())
 
     res = optimal_parameters(profile_customer, customer)
-
+    # res = optimal_parameters_5(profile_customer, customer)
     print('fitting res=(a,b): ', res)
-    est_exp_load = res[0]*customer.sum()*profile_customer.pow(res[1])
+    a, b = res
+    SJV = customer.sum()
+    max_error = SJV/100*(a*(profile_customer.max()**b) -
+                         customer.max()/SJV)
+    print('max diff: ', max_error)
+    est_exp_load = a*customer.sum()*(profile_customer.pow(b))
     est_lin_load = customer.sum()*profile_customer
 
     error_exp = np.abs(est_exp_load - customer)
@@ -62,37 +92,55 @@ for customer_id in customers.index:
     absolute_error_exp = np.sqrt(np.square(error_exp).sum())
     absolute_error_lin = np.sqrt(np.square(error_lin).sum())
 
-    print("Absolute error exp fit: ")
-    print(absolute_error_exp)
-    print("Absolute error lin fit: ")
-    print(absolute_error_lin)
     print('relative improvement: ')
     improvement = (1 - absolute_error_exp/absolute_error_lin)*100
     improvement_lis[count] = improvement
     if improvement > 0:
-        better.append((improvement, res))
-
+        better.append((customer_id, improvement, res))
+        print('better: ')
     else:
-        worse.append((improvement, res))
+        worse.append((customer_id, improvement, res))
+        print('worse: ')
     count += 1
+    print("{:.4f}%".format((1 - absolute_error_exp/absolute_error_lin)*100))
+
+
+    plt.figure(figsize=(12, 5))
+    plt.xlabel('timestamp every 15 min')
+    plt.ylabel('KWH usage')
+
+    plt.title("customer {} from profile {}".format(
+        customer_id, profile_type))
+    ax1 = est_exp_load.plot.line(color='red', grid=True,
+                                 label="estimated load exponential",
+                                 alpha=0.8)
+    ax2 = est_lin_load.plot.line(color='green', grid=True,
+                                 label="estimated load linear",
+                                 alpha=0.5)
+    ax3 = customer.plot.line(color='blue', grid=True,
+                             label="real load",
+                             alpha=0.3)
+
+    plt.legend()
+    plt.show()
     print("{:.4f}%".format((1 - absolute_error_exp/absolute_error_lin)*100))
     print()
     print()
 
 print('improvements: ')
-print(improvement_lis)
+print(improvement_lis[:10])
 print('better lis: ')
-print(better)
+print(better[:10])
 print('better sum: ')
-print(sum([x for (x,_) in better]))
+print(sum([x for (_, x, _) in better]))
 # print('length {} and sum {}'.format(len(better), sum(better)))
 print()
 
 print('worse lis: ')
-print(worse)
+print(worse[:10])
 print('worse sum:')
-print(sum([x for (x,_) in worse]))
-#print('length {} and sum {}'.format(len(worse), sum(worse)))
+print(sum([x for (_, x, _) in worse]))
+# print('length {} and sum {}'.format(len(worse), sum(worse)))
 plt.hist(improvement_lis)
 plt.show()
 # plt.show(block=True)
--- load_data_scripts/save_unique_profile_customer.py
+++ load_data_scripts/save_unique_profile_customer.py
@@ -3,14 +3,18 @@ from .load_save_profiles import load_profiles
 
 
 def unique_customer(reload=0, Alliander_path="../Alliander_data/",
-                    nrows=100):
+                    nrows=None):
     try:
         if reload:
             raise FileNotFoundError
+        columns = pd.read_csv(Alliander_path + 'unique_meetdata.csv',
+                              nrows=1).columns.tolist()
+        raw_dtypes = dict((column, 'float32') for column in columns)
         unique_df = pd.read_csv(
             Alliander_path + 'unique_meetdata.csv',
             index_col=0,
-            nrows=nrows)
+            nrows=nrows,
+            dtype=raw_dtypes)
         unique_df.columns = pd.to_datetime(unique_df.columns)
     except FileNotFoundError:
         connect_df, _ = load_profiles(reload=reload,
@@ -18,11 +22,18 @@ def unique_customer(reload=0, Alliander_path="../Alliander_data/",
         get_indexes = connect_df.index.drop_duplicates(
             keep=False).append(pd.Index([0]))
 
+        columns = pd.read_csv(Alliander_path + 'gv_meetdata_select.csv',
+                              nrows=1).columns.tolist()
+        raw_dtypes = dict((column, 'float32') for column in columns)
+
         unique_df = pd.read_csv(Alliander_path + 'gv_meetdata_select.csv',
                                 skiprows=lambda x: x not in get_indexes,
                                 index_col=0,
+                                dtype=raw_dtypes,
                                 nrows=nrows)
+
         unique_df.columns = pd.to_datetime(unique_df.columns)
+        unique_df = unique_df.dropna()
         unique_df.to_csv(Alliander_path + 'unique_meetdata.csv')
     return unique_df
 

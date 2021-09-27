c25600433564c2cef000019edd7e184a416d9e0e
francescogili <66681714+francescogili@users.noreply.github.com>
2021-09-27 13:28:25 +0200

    Update exponential_fitting.py

--- fitting_profiles/exponential_fitting.py
+++ fitting_profiles/exponential_fitting.py
@@ -6,8 +6,8 @@ from datetime import datetime
 from scipy.optimize import fsolve, minimize
 import math
 
-profile = pd.read_csv('C:/Users/tratt/OneDrive/Desktop/Internship Alliander/Alliander_data/neat_profiles.csv')
-customer = pd.read_csv('C:/Users/tratt/OneDrive/Desktop/Internship Alliander/Alliander_data/unique_meetdata.csv', index_col = 0, nrows = 200)
+profile = pd.read_csv('../Alliander_data/neat_profiles.csv')
+customer = pd.read_csv('../Alliander_data/unique_meetdata.csv', index_col = 0, nrows = 200)
 
 '''
 
@@ -35,7 +35,7 @@ def set_correct_index_profiles(profile_df, meetdata_df):
 # Delete first
     profile_df = profile_df.drop([first_column], axis=1)
     
-    profile_df.to_csv('C:/Users/tratt/OneDrive/Desktop/Internship Alliander/Alliander_data/neat_profiles_fitted.csv')
+    profile_df.to_csv('../Alliander_data/neat_profiles_fitted.csv')
     return profile_df
 
 print(set_correct_index_profiles(profile,customer).head())    
